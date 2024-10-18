6a3e8523cd3da0e5df70fa080b80969802c75793
Megumii05 <134986037+Megumii05@users.noreply.github.com>
2024-10-18 22:10:43 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
   titleHeader: "From boy khối 1 with luv 🥰",
   descriptionHeader:
-    "Hellu các cậu nhé, đây cũng là 20/10 thứ 2 chúng mình bên nhau rồi. Trong suốt hơn 1 năm qua, chúng mình đã cùng nhau trải qua biết bao kỉ niệm và các cậu luôn là hậu phương vững chắc, là động lực và là chỗ dựa của chúng tớ mỗi khi lên lớp. Năm nay thì chúng mình 0 còn học cùng với nhau đông đủ như hồi năm nhất nữa, chúng tớ muốn tổ chức cho các cậu 1 buổi hoàng tráng lắm nhưng đợt này thi cử cũng nhiều, lại trùng với cả FCD nữa nên tập hợp đủ ae hơi khó. Vì thế chúng tớ xin chúc các cậu 1 ngày 20/10 và cả những ngày khác nữa luôn xinh đẹp, giỏi giang, nhiều sức khỏe, chúc các cậu 8386, mãi đỉnh và luôn là những người phụ nữ tuyệt vời nhất trong lòng chúng tớ nhé^^" </br> Để tạ lỗi vì không thể tổ chức được, chúng tớ có 1 món quà nho nhỏ muốn gửi đến các cậu 😭 Các cậu nhận cho tớ vui nhé^^",
+    "Hellu các cậu nhé, đây cũng là 20/10 thứ 2 chúng mình bên nhau rồi. Trong suốt hơn 1 năm qua, chúng mình đã cùng nhau trải qua biết bao kỉ niệm và các cậu luôn là hậu phương vững chắc, là động lực và là chỗ dựa của chúng tớ mỗi khi lên lớp. Năm nay thì chúng mình 0 còn học cùng với nhau đông đủ như hồi năm nhất nữa, chúng tớ muốn tổ chức cho các cậu 1 buổi hoàng tráng lắm nhưng đợt này thi cử cũng nhiều, lại trùng với cả FCD nữa nên tập hợp đủ ae hơi khó. Vì thế chúng tớ xin chúc các cậu 1 ngày 20/10 và cả những ngày khác nữa luôn xinh đẹp, giỏi giang, nhiều sức khỏe, chúc các cậu 8386, mãi đỉnh và luôn là những người phụ nữ tuyệt vời nhất trong lòng chúng tớ nhé^^. </br> Để tạ lỗi vì không thể tổ chức được, chúng tớ có 1 món quà nho nhỏ muốn gửi đến các cậu 😭 Các cậu nhận cho tớ vui nhé^^",
   buttonYes: "Đồng ý",
   buttonNo: "Không đồng ý",
   titleModar: "Hjhj tớ bt các cậu sẽ 0 từ chối mà^^",
   descriptionModar:
-    "các cậu ấn vào link này nhé", 
+    "các cậu ấn vào link này nhé.", 
 };
